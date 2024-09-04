035f25d1c081120613a0c2fe3d4a4d9ac9f371f3
PRATHAM SHIROL <105107078+prathamshirol@users.noreply.github.com>
2024-09-04 14:25:48 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (34.39%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,38 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        // Trim leading and trailing spaces
-        s = trimFirstAndLastSpaces(s);
-        
-        int c = 0;
-        int n = s.length(); // Get the length of the string
-        
-        // Start from the end of the string and move backwards
-        for (int i = n - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ') {
-                c++;
-            } else if (c > 0) { 
-                break;
-            }
-        }
-        
-        return c;
-    }
+      
+        int end = s.length() - 1;
 
-    private String trimFirstAndLastSpaces(String str) {
-        int start = 0;
-        int end = str.length() - 1;
-        
-        // Move start forward until the first non-space character
-        while (start <= end && str.charAt(start) == ' ') {
-            start++;
-        }
-        
-        // Move end backward until the last non-space character
-        while (end >= start && str.charAt(end) == ' ') {
+        while (end >= 0 && s.charAt(end) == ' ') {
             end--;
         }
-        
-        // Return the trimmed substring
-        return str.substring(start, end + 1);
+
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+
+        return end - start;  
     }
 }
