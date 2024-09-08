1e3169909262a66399a5d45fee58d31ad7e9901b
PRATHAM SHIROL <105107078+prathamshirol@users.noreply.github.com>
2024-09-08 23:44:43 +0530

    Time: 2 ms (48.10%), Space: 8.5 MB (7.48%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int count = 0;
+        int c = 0;
+        string s1, s2;
 
-    string temp1 = bitset<32>(x).to_string();
-   string temp2 =  bitset<32>(y).to_string();    // num change ho gya bit m  firr usse string m change kar dia
-        
-        for(int i = 0;i<temp1.size();i++){
-           
-                if(temp1[i] != temp2[i]){
-                    count++;
-                }
-            
+        // Convert x to binary string
+        while (x > 0) {
+            s1 = char((x & 1) + '0') + s1;
+            x = x >> 1;
         }
-        return count;
-      
 
+        // Convert y to binary string
+        while (y > 0) {
+            s2 = char((y & 1) + '0') + s2;
+            y = y >> 1;
+        }
 
-     
+        // Ensure both strings have the same length by padding with leading zeros
+        while (s1.size() < s2.size()) s1 = '0' + s1;
+        while (s2.size() < s1.size()) s2 = '0' + s2;
+
+        // Count the number of differing bits
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] != s2[i]) {
+                c++;
+            }
+        }
+
+        return c;
     }
 };
