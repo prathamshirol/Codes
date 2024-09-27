c4517c9f2ab2e22688a43a5811e8a4e7222284ed
PRATHAM SHIROL <105107078+prathamshirol@users.noreply.github.com>
2024-09-27 11:15:12 +0530

    Time: 19 ms (48.71%), Space: 27.2 MB (39.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int> m;
+      map<int,int> m;
         for(int i=0;i<nums.size();i++)
         {
             m[nums[i]]++;
         }
+        int k=0;
+        int p=0;
         for(const auto & pair:m)
         {
-            if(pair.second>nums.size()/2)
-                return pair.first;
+            if(pair.second>k)
+            {
+                k=pair.second;
+                p=pair.first;
+            }
         }
-        return -1;
+        return p;;
     }
 };
